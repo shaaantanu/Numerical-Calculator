42ae25602fe2882634c995d7ba3ceea49d680f97
shaantanu <shantanus2410@gmail.com>
2024-01-12 11:31:48 +0530

    update commit

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ function appendOperator(operator) {
     document.getElementById('display').value += operator;
 }
 
+let calculationHistory = [];
+
 function calculate() {
     var expression = document.getElementById('display').value;
 
@@ -26,7 +28,12 @@ function calculate() {
         }
 
         document.getElementById('display').value = result;
+        calculationHistory.push(result);
     } catch (error) {
         document.getElementById('display').value = 'Error';
     }
+}
+
+function showHistory() {
+    alert('Calculator History:\n' + calculationHistory.join('\n'));
 }
